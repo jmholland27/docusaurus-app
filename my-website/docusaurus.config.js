1cd9444f473bcc2fa0112c78178e7df882ad67c2
Jake Holland <jmholland27@gmail.com>
2025-11-09 13:05:09 -0500

    Updated link

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://jmholland27.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/my-website',
+  baseUrl: '/docusaurus-app',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
